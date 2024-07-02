7d63883b67195bc628057ff1783f256cc2121ae1
cellothere <109959547+cellothere@users.noreply.github.com>
2024-07-02 13:49:47 -0400

    Adding vercel config

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+//const cors = require('cors')
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+// app.use(cors(
+//   {
+//   origin: ["https://cello-repertoire-initiative-1whq.vercel.app"],
+//   methods: ["POST", "GET"],
+//   credentials: true
+//   }
+// ))
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
