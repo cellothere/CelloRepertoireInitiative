8e13e951cf3fc063667af198b96107b110454a42
cellothere <109959547+cellothere@users.noreply.github.com>
2024-07-02 14:54:26 -0400

    gitignore

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors(
   {
-  origin: ["https://cello-repertoire-initiative-api.vercel.app"],
+  origin: [""],
   methods: ["POST", "GET"],
   credentials: true
   }
