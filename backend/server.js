fafcbd7563545c9a58b3f5619bb3d2fd0f685bb8
cellothere <109959547+cellothere@users.noreply.github.com>
2024-07-02 15:02:27 -0400

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors(
   {
-  origin: ["https://cello-repertoire-initiative-frontend.vercel.app/"],
+  origin: ["https://cello-repertoire-initiative-frontend-3rqssrojt.vercel.app/"],
   methods: ["POST", "GET"],
   credentials: true
   }
