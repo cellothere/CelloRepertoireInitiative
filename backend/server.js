40747d86df3df4b28d5ceefbad37ee08fdebfe20
cellothere <109959547+cellothere@users.noreply.github.com>
2024-07-03 12:51:14 -0400

    comments

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require('cors') //cors
+const cors = require('cors')
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use(cors())
+app.use(cors()) //configure in the vercel config
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
